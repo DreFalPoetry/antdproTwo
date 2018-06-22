a1c24a1b1a1c404d894c599485c4d3bbe370ce7b
Racky <racky.liu@moca-tech.net>
2018-06-22 17:23:44 +0800

    dfef

--- src/common/menu.js
+++ src/common/menu.js
@@ -2,45 +2,45 @@ import { isUrl } from '../utils/utils';
 
 const menuData = [
   {
-    name: 'dashboard',
-    icon: 'dashboard',
-    path: 'dashboard',
-    children: [
-      {
-        name: '分析页',
-        path: 'analysis',
-      },
-      {
-        name: '监控页',
-        path: 'monitor',
-      },
-      {
-        name: '工作台',
-        path: 'workplace',
-        // hideInBreadcrumb: true,
-        // hideInMenu: true,
-      },
-    ],
+        name: 'dashboard',
+        icon: 'dashboard',
+        path: 'dashboard',
+        children: [
+        {
+            name: '分析页',
+            path: 'analysis',
+        },
+        {
+            name: '监控页',
+            path: 'monitor',
+        },
+        {
+            name: '工作台',
+            path: 'workplace',
+            // hideInBreadcrumb: true,
+            // hideInMenu: true,
+        },
+        ],
   },
   {
-    name: '表单页',
-    icon: 'form',
-    path: 'form',
-    children: [
-      {
-        name: '基础表单',
-        path: 'basic-form',
-      },
-      {
-        name: '分步表单',
-        path: 'step-form',
-      },
-      {
-        name: '高级表单',
-        authority: 'pm,admin',
-        path: 'advanced-form',
-      },
-    ],
+        name: '表单页',
+        icon: 'form',
+        path: 'form',
+        children: [
+        {
+            name: '基础表单',
+            path: 'basic-form',
+        },
+        {
+            name: '分步表单',
+            path: 'step-form',
+        },
+        {
+            name: '高级表单',
+            authority: 'pm,admin',
+            path: 'advanced-form',
+        },
+        ],
   },
   {
     name: '列表页',
